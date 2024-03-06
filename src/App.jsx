8cd58e123fc0cc6a2f02ee220c08a15f426de437
SuhaanTonse <suhaanthonse22@gmail.com>
2024-03-06 21:39:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItems from "./components/TodoItems";
+import WelcomeMessage from "./components/WelcomeMessage";
+import "./App.css";
+import TodoItemsContextProvider, {
+  TodoItemsContext,
+} from "./store/todo-items-store";
+
+function App() {
+  return (
+    <TodoItemsContextProvider>
+      <center className="todo-container">
+        <AppName></AppName>
+        <AddTodo></AddTodo>
+
+        <WelcomeMessage></WelcomeMessage>
+
+        <TodoItems></TodoItems>
+      </center>
+    </TodoItemsContextProvider>
+  );
+}
+
+export default App;
